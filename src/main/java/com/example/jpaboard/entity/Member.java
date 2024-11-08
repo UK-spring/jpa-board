a29fcc639502ceac9b80b8883aa5edb760a7ae9d
UK <hwangwonuk94@gmail.com>
2024-11-09 07:04:31 +0900

    feat: Member 제약조건 설정

--- src/main/java/com/example/jpaboard/entity/Member.java
+++ src/main/java/com/example/jpaboard/entity/Member.java
@@ -10,8 +10,10 @@ public class Member extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String username;
 
+    @Column(nullable = false)
     private String password;
 
     private Integer age;
