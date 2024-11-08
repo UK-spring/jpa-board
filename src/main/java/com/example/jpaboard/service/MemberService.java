34006bf97f54d78ecb79c3a0ba2b909b06ce9a9c
UK <hwangwonuk94@gmail.com>
2024-11-09 08:36:58 +0900

    feat: Member 조회 API

--- src/main/java/com/example/jpaboard/service/MemberService.java
+++ src/main/java/com/example/jpaboard/service/MemberService.java
@@ -1,10 +1,15 @@
 package com.example.jpaboard.service;
 
+import com.example.jpaboard.dto.MemberResponseDto;
 import com.example.jpaboard.dto.SignUpResponseDto;
 import com.example.jpaboard.entity.Member;
 import com.example.jpaboard.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +24,20 @@ public class MemberService {
         Member savedMember = memberRepository.save(member);
 
         return new SignUpResponseDto(savedMember.getId(), savedMember.getUsername(), savedMember.getAge());
+    }
+
+    public MemberResponseDto findById(Long id) {
+
+        Optional<Member> optionalMember = memberRepository.findById(id);
+
+        // NPE 방지
+        if (optionalMember.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Does not exist id = " + id);
+        }
+
+        Member findMember = optionalMember.get();
+
+        return new MemberResponseDto(findMember.getUsername(), findMember.getAge());
     }
 
 }
